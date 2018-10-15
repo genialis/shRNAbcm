9449c49acd5fa01af89573c13fde4b6b82237610
Roman Luštrik <romunov@gmail.com>
2018-10-15 13:19:18 +0200

    Add summaryTable function

--- R/summaryTable.R
+++ R/summaryTable.R
@@ -1,31 +1,52 @@
-#' Summary table
+#' Summary and count table of shRNA by sample
 #'
-#' Produce a summary report.
+#' Produce a summary report. Report will include data for total number of reads, 5' trimmed reads, 3' trimmed reads,
+#' number and percent of mapped reads, number and percent of perfectly mapped reads, number and percent of good
+#' mapped reads, number and percent of poorly mapped reads and number of unmapped reads.
 #'
-#' @param samkey
-#' @param lib
-#' @param trimreport
+#' @param samkey Character. A link to sample key file. See source code for how the data should be structured.
+#' @param lib Character. A link to library (fasta) file. See source code for how the data should be structured.
+#' @param trimreport Character. A link to global trim report from the pipeline. See source code for how the data
+#' should be structured.
+#' @param mapping.path Character. Path to where mapping files reside.
+#' @param mapping.rx Character. Valid regular expression for collating of mapping files (per sample).
+#' @param wt_alignscores Integer. Perfect align counts. Align counts that have align scores equal to \code{wt_alignscores} and
+#' read lengths equal or higher to \code{wt_readlengths_lower} will be summed.
+#' @param wt_readlengths_lower Integer. Perfect align counts. Align counts that have align scores equal to \code{wt_alignscores}
+#' and read lengths equal or higher to \code{wt_readlengths_lower} will be summed.
+#' @param high_alignscores_lower,high_alignscores_upper,high_readlengths_lower Integer. Good align counts. Align counts with align
+#' scores >= \code{high_alignscores_lower} and < \code{high_alignscores_upper} and read lengths equal or higher than
+#' \code{high_readlengths_lower} will be summed.
+#' @param low_alignscores_upper,low_readlengths_upper Integer. Poor align counts. Align counts below \code{low_alignscores_upper}
+#' or read length below \code{low_readlengths_upper} will be excluded from the tally.
+#' @param count_alignscores,count_readlengths Integer. For count matrix, align counts for which align scores equal or greater to
+#' \code{count_alignscores} and read lengths equal or greater to \code{count_readlengths} will be used in the final tally.
 #'
-#' @retrun Two files, \code{global_alignment_report.txt} and \code{count_matrix.txt}.
+#' @return Two files, \code{global_alignment_report.txt} and \code{count_matrix.txt} are written as a side effect. Function
+#' returns a list of length 2 where one can access alignment report (\code{output$alignment_report}) or count matrix
+#' (\code{output$count_matrix}).
 #'
 #' @export
-#' @importFrom data.table fread
 #'
-#' @author Roman Lustrik (roman@@genialis.com), original idea Nicholas Neill (nicholas.neill@@bcm.edu )
+#' @author Roman Luštrik (roman@@genialis.com) adapted original code by Nicholas Neill (nicholas.neill@@bcm.edu).
 
-summaryTable <- function(samkey, lib, trimreport, wt_alignscores = 0, wt_readlengths_lower = 26,
-                         high_alignscores_upper = 0, high_alignscores_lower = -6,
-                         high_readlengths_lower = 26, low_alignscores_upper = -6,
-                         low_readlengths_upper = 26, output = "global_alignment_report.txt") {
+summaryTable <- function(samkey, lib, trimreport, mapping.path = ".", mapping.rx = "^.*_mapped_species\\.txt$",
+                         wt_alignscores = 0, wt_readlengths_lower = 26,
+                         high_alignscores_upper = 0, high_alignscores_lower = -6, high_readlengths_lower = 26,
+                         low_alignscores_upper = -6, low_readlengths_upper = 26,
+                         count_alignscores = -6, count_readlengths = 26,
+                         output_report = "global_alignment_report.txt",
+                         output_count = "count_matrix.txt") {
+
   # Import data ####
-  samples <- fread(samkey)
+  samples <- read.table(samkey, header = TRUE)
   #        sample dox tam rep
   # 1: TB4445-17 unt unt   2
   # 2: TB4445-18 unt unt   3
   # 3: TB4445-19 unt unt   4
   # 4: TB4445-20 dox unt   1
 
-  lib <- fread(lib, header = FALSE, stringsAsFactors = FALSE)
+  lib <- read.table(lib, header = FALSE, stringsAsFactors = FALSE)
   lib$seq <- 1:2
   lib <- unstack(lib)
   lib$X1 <- gsub("^>", "", lib$X1)
@@ -36,9 +57,24 @@ summaryTable <- function(samkey, lib, trimreport, wt_alignscores = 0, wt_readlen
   # 3  AANAT_304-AANAT GTATAGAAGGGTACCAGCGCGTCCTTGCC
   # 4 AANAT_3349-AANAT GTATGAAGCTGAACCTCTCATAGAATGCC
 
-  trim.data <- fread(trimreport, header = FALSE, stringsAsFactors = FALSE, sep = NULL)
+  trim.data <- read.table(trimreport, header = FALSE, stringsAsFactors = FALSE, sep = NULL)
+  #          V1
+  # 1   sample1
+  # 2 2,345,004
+  # 3 2,319,009
+  # 4 1,718,789
+
   trim.data$seq <- 1:4
   trim.data <- unstack(trim.data)
+
+  # In case only one sample is handled, unstack will not behave as expected and
+  # t() is needed.
+  if (all(nrow(trim.data) == 4 & ncol(trim.data) == 1)) {
+    trim.data <- data.frame(t(trim.data))
+  }
+
+  rownames(trim.data) <- NULL
+
   trim.data[, 2:4] <- sapply(trim.data[, 2:4], gsub, pattern = ",", replacement = "")
   colnames(trim.data) <- c("sample", "total", "trimmed_5", "trimmed_3")
   trim.data$total <- as.numeric(trim.data$total)
@@ -53,7 +89,7 @@ summaryTable <- function(samkey, lib, trimreport, wt_alignscores = 0, wt_readlen
   # 6 TB4445-22 2680203   2646307   1779339
 
   # Import align counts and mapped shRNAs ####
-  file.samples <- paste(samples$sample, "mapped_species.txt", sep = "_")
+  file.samples <- list.files(path = mapping.path, pattern = mapping.rx, full.names = TRUE)
 
   xy <- lapply(file.samples, FUN = function(i) {
     ri <- read.table(i, header = FALSE, sep = "", stringsAsFactors = FALSE)
@@ -63,88 +99,95 @@ summaryTable <- function(samkey, lib, trimreport, wt_alignscores = 0, wt_readlen
     ri
   })
   names(xy) <- samples$sample
+  # This result is a list, where each element is mapped shRNAs for a given sample.
+  # One sample may look something like this:
+  #   align_counts    mapped_shRNAs                     sequences align_scores read_lengths
+  # 1           28 AANAT_2131-AANAT GTATGAGGCAGCGAAACTCACTGGCTGCC            0           29
+  # 2            3 AANAT_2131-AANAT   GGCAGCGAGTACAAATCGACTGGATAC          -16           27
+  # 3            1 AANAT_2131-AANAT GTATGAGGCAGCGAAACCCACTGGCTGCC           -2           29
+  # 4            1 AANAT_2131-AANAT GTATGAGGCAGCGAAACGCACTGGCTGCC           -2           29
+  # 5           30 AANAT_2740-AANAT      GTATCAGAACATCAGGATGGTGCC          -12           24
+  # 6           20 AANAT_2740-AANAT   GTATTTTCCACAGCATGACTCGGTGCC          -14           27
 
-  trim.data$mapped <- sapply(xy, FUN = function(x) sum(x$align_counts))
+  # Calculate statistics. ####
+  trim.data$mapped <- sapply(xy, FUN = function(x) sum(x$align_counts, na.rm = TRUE))
   trim.data["mapped %"] <- signif(with(trim.data, mapped / total), 3)
 
   trim.data$perfect <- sapply(xy, FUN = function(x) sum(x[x$align_scores == wt_alignscores
-                                           & x$read_lengths >= wt_readlengths_lower, "align_counts"]))
+                                                          & x$read_lengths >= wt_readlengths_lower, "align_counts"],
+                                                        na.rm = TRUE))
   trim.data["perfect %"] <- signif(with(trim.data, perfect / total), 3)
 
   trim.data$good <- sapply(xy, FUN = function(x) sum(x[x$align_scores < high_alignscores_upper
-                                               & x$align_scores >= high_alignscores_lower
-                                               & x$read_length >= high_readlengths_lower, "align_counts"]))
+                                                       & x$align_scores >= high_alignscores_lower
+                                                       & x$read_length >= high_readlengths_lower, "align_counts"],
+                                                     na.rm = TRUE))
   trim.data["good %"] <- signif(with(trim.data, good / total), 3)
 
-  trim.data$poor <- sapply(xy, FUN = function(x) sum(x[x$align_scores < low_alignscores_upper | x$read_lengths < low_readlengths_upper, "align_counts"]))
+  trim.data$poor <- sapply(xy, FUN = function(x) sum(x[x$align_scores < low_alignscores_upper
+                                                       | x$read_lengths < low_readlengths_upper,
+                                                       "align_counts"],
+                                                     na.rm = TRUE))
   trim.data["poor %"] <- signif(with(trim.data, poor / total), 3)
 
   trim.data$unmapped <- with(trim.data, total - mapped)
   trim.data["unmapped %"] <- signif(with(trim.data, unmapped / total), 3)
 
-  write.table(trim.data, file = output, sep = '\t', row.names = FALSE)
+  # sample   total trimmed_5 trimmed_3  mapped mapped % perfect perfect %   good good %   poor poor % unmapped unmapped %
+  # 1 TB4445-17 2345004   2319009   1718789 1712494    0.730  970717     0.414 343041 0.1460 398736  0.170   632510      0.270
+  # 2 TB4445-18 2603252   2576740   1927294 1897598    0.729 1086360     0.417 378654 0.1450 432584  0.166   705654      0.271
+  # 3 TB4445-19 3358655   3336617   2670632 2630073    0.783 1490566     0.444 530136 0.1580 609371  0.181   728582      0.217
+  # 4 TB4445-20 3260564   3198344   1671554 1637325    0.502  938649     0.288 321022 0.0985 377654  0.116  1623239      0.498
+  # 5 TB4445-21 2078391   2059003   1527371 1503861    0.724  855002     0.411 303352 0.1460 345507  0.166   574530      0.276
+  # 6 TB4445-22 2680203   2646307   1779339 1749226    0.653  998534     0.373 353688 0.1320 397004  0.148   930977      0.347
+  # Write result of global alignment report. ####
+  write.table(trim.data, file = output_report, sep = '\t', row.names = FALSE)
 
-  trim.data
+  # Create count matrix ####
+  # Create new object which will be one big table of all the data. Add a new column
+  # which will designate from which sample individual shRNA comes from. This can now
+  # be used for further calculation using R-way logic.
+  cnm <- do.call(rbind, xy)
+  rownames(cnm) <- NULL
+
+  cnm$sample <- rep(names(xy), times = sapply(xy, nrow))
+
+  # Keep only those shRNAs which satisfy the criteria.
+  cnm <- cnm[cnm$align_scores >= count_alignscores & cnm$read_lengths >= count_readlengths, ]
+
+  # Aggreggate align_counts (sum) according to sample and shRNA.
+  cnm.agg <- aggregate(align_counts ~ mapped_shRNAs + sample, data = cnm, FUN = sum)
+  #      mapped_shRNAs    sample align_counts
+  # 1 AANAT_2131-AANAT TB4445-17           30
+  # 2 AANAT_2740-AANAT TB4445-17            5
+  # 3  AANAT_304-AANAT TB4445-17           47
+  # 4 AANAT_3349-AANAT TB4445-17          244
+  # 5 AANAT_3958-AANAT TB4445-17          114
+  # 6 AANAT_4567-AANAT TB4445-17           12
+
+  # Reshape data to wide format so that rows are shRNAs and columns are samples.
+  cnm.agg <- reshape(data = cnm.agg, timevar = "sample", idvar = "mapped_shRNAs", direction = "wide")
+  names(cnm.agg) <- gsub("align_counts\\.", "", names(cnm.agg))
+  # mapped_shRNAs TB4445-17 TB4445-18 TB4445-19 TB4445-20 ...
+  # 1 AANAT_2131-AANAT        30        77         4      ...
+  # 2 AANAT_2740-AANAT         5        13        15      ...
+  # 3  AANAT_304-AANAT        47        26        73      ...
+  # 4 AANAT_3349-AANAT       244       297       181      ...
+  # 5 AANAT_3958-AANAT       114        90       237      ...
+  # 6 AANAT_4567-AANAT        12        26        39      ...
+
+  # This step is needed so that all shRNAs that appear in output of mapped reads are included.
+  cnm.agg <- merge(x = lib[, "shRNA", drop = FALSE], y = cnm.agg,
+                   by.x = "shRNA", by.y = "mapped_shRNAs",
+                   all.x = TRUE)
+
+  # Note that for some samples, shRNA may be missing, in which case, reshape will add an NA value.
+  # This can indicate two things - that there were no reads for this shRNA OR they have been
+  # filtered out. In any case, the below line will replace all NAs to keep up with the specs.
+  cnm.agg[is.na(cnm.agg)] <- 0
+
+  # Write data to file and return a list of calculated data.
+  write.table(cnm.agg, file = output_count, sep = "\t", row.names = FALSE, quote = FALSE)
+
+  return(list(alignment_report = trim.data, count_matrix = cnm.agg))
 }
-#
-## generate a table of read species statistics for each hairpin
-## $1 is path to sample key
-## $2 is path to library file (trimmed fasta)
-## $3 is path to global trim report
-
-# args <- c(sample = "sample_key_gata3.txt",
-# library = "chromatin_library.fasta",
-# trimreport = "global_trim_report.txt"
-# )
-
-# samples = as.matrix(read.table(args[1], header = T, sep = '\t', stringsAsFactors = F))
-# sample_id = samples[,1]
-# library = read.table(args[2], sep = '\t', stringsAsFactors = F)
-# library = cbind(library, rep(c(1,2)))
-# library = unstack(library)
-# shrna = library[,1]
-# shrna = gsub('>', '', shrna, fixed = T)
-
-#make alignment report for each sample
-# print('Generating alignment report.')
-# trim_data = read.table(args[3], stringsAsFactors = F)
-# trim_data[[2]] = rep(1:4, times = nrow(samples))
-# trim_data = unstack(trim_data)
-# total = as.numeric(gsub(',', '', trim_data[[2]], fixed = T))
-# trimmed_5 = as.numeric(gsub(',', '', trim_data[[3]], fixed = T))
-# trimmed_3 = as.numeric(gsub(',', '', trim_data[[4]], fixed = T))
-# align_counts = lapply(sample_id, function(x) as.numeric(read.table(paste(x, '_mapped_species.txt', sep = ''))[[1]]))
-# mapped_shrnas = lapply(sample_id, function(x) read.table(paste(x, '_mapped_species.txt', sep = ''))[[2]])
-# read_seqs = lapply(sample_id, function(x) as.character(read.table(paste(x, '_mapped_species.txt', sep = ''))[[3]]))
-# read_lengths = lapply(read_seqs, nchar)
-# align_scores = lapply(sample_id, function(x) read.table(paste(x, '_mapped_species.txt', sep = ''))[[4]])
-# align_scores = lapply(align_scores, function(x) as.numeric(gsub('AS:i:', '', x, fixed = T)))
-# mapped = sapply(align_counts, sum)
-# wt = mapply(function(x,y,z) sum(x[y == 0 & z >= 26]), align_counts, align_scores, read_lengths)
-# high_q = mapply(function(x,y,z) sum(x[y < 0 & y >= -6 & z >= 26]), align_counts, align_scores, read_lengths)
-# low_q = mapply(function(x,y,z) sum(x[y < -6 | z < 26]), align_counts, align_scores, read_lengths)
-# unmapped = total - mapped
-# mapped_pc = signif(mapped / total, 3)
-# wt_pc = signif(wt / total, 3)
-# high_q_pc = signif(high_q / total, 3)
-# low_q_pc = signif(low_q / total, 3)
-# unmapped_pc = signif(unmapped / total, 3)
-# summary = data.frame(sample_id,  total,   trimmed_5  , trimmed_3,   mapped,  mapped_pc,      wt,        wt_pc,   high_q, high_q_pc, low_q, low_q_pc,  unmapped, unmapped_pc)
-# colnames(summary) = c('sample', 'total', 'trimmed_5', 'trimmed_3', 'mapped', 'mapped_%', 'perfect', 'perfect_%', 'good', 'good_%', 'poor', 'poor_%', 'unmapped', 'unmapped_%')
-
-
-
-
-
-# write.table(summary, file = 'original_global_alignment_report.txt', sep = '\t', row.names = F)
-#
-# #make count matrix
-# print('Generating count matrix.')
-# good_ind = mapply(function(x,y) which(x >= -6 & y >= 26), align_scores, read_lengths)
-# good_counts = mapply(function(x,y) x[y], align_counts, good_ind)
-# good_mappings = mapply(function(x,y) x[y], mapped_shrnas, good_ind)
-# count_matrix = mapply(function(x,y) sapply(shrna, function(z) sum(x[y == z])), good_counts, good_mappings)
-# colnames(count_matrix) = sample_id
-#
-# # TODO: should this matrix be saved for chromatin, kinase and ubiquitin?
-# write.table(count_matrix, file = 'original_count_matrix.txt', sep = '\t')
---
+++ tests/testthat/test-summarytable.R
@@ -0,0 +1,83 @@
+context("Testing summaryTable")
+
+library(shRNAbcm)
+
+file.sample <- "sample1_test_spec.txt"
+file.samplekey <- "samplekey.txt"
+file.lib <- "library.txt"
+file.trimreport <- "trimreport.txt"
+
+# On exit, the test will clean after itself.
+on.exit({
+  unlink(c(file.sample, file.samplekey, file.lib, file.trimreport))
+})
+
+smp <- read.table(text = "      1 AANAT_2131-AANAT	GTATAAGGCAGCGATGGTGAGCTGCC	AS:i:-14
+      1 AANAT_2131-AANAT	GTATGAGGCAGCGAAACTCACTGGCTGCC	AS:i:0
+      1 AANAT_2740-AANAT	GTATTACCTTCAACGATGGTGCCCCTGCC	AS:i:-16
+      1 AANAT_2740-AANAT	GTATTTATCCACAGCAATGATCGGTGCC	AS:i:-16
+      1 AANAT_2740-AANAT	GTATTTCCACAGCAATGACTCGGTGCC	AS:i:-14
+      1 AANAT_2740-AANAT	GTTAAACATGATGGGTCCATGCTGCC	AS:i:-18
+     60 AANAT_304-AANAT	GTATAGAAGGGTACCAGCGCGTCCTTGCC	AS:i:0
+     10 AANAT_304-AANAT	GTATAGAGGGGTACCAGCGCGTCCTTGC	AS:i:-2
+      6 AANAT_304-AANAT	GTATAGAGGGGTACCAGCGCGTCCTTGCC	AS:i:-2
+      1 AANAT_304-AANAT	GTATAGAAGGGTACCAGCGCGTACTTGCC	AS:i:-2
+      1 AANAT_304-AANAT	GTATAGAAGGGTACCAGCGCGTCATTGCC	AS:i:-2
+      1 AANAT_304-AANAT	GTATAGAAGGGTACCAGCGCGTCCTTGTC	AS:i:-2
+      1 AANAT_304-AANAT	GTATAGAAGGGTACCGGCGCGTCCTTGCC	AS:i:-2
+    132 AANAT_3349-AANAT	GTATGAAGCTGAACCTCTCATAGAATGCC	AS:i:0
+     24 AANAT_3349-AANAT	GTATGGAATAGAAGTCTCAAAGAGTACC	AS:i:-18
+      9 AANAT_3349-AANAT	GTATGGAATAGAAGTCTCAAAGAGTGCC	AS:i:-16
+      5 AANAT_3349-AANAT	GTATGAAGCTGAACCTCTCATAGATGCC	AS:i:-2
+      3 AANAT_3349-AANAT	GTATGAAGCTGAACCACTCATAGAATGCC	AS:i:-2
+      1 AANAT_3349-AANAT	ATATGAAGCTGAACCTCTCATAGAATGCC	AS:i:-2
+      1 AANAT_3349-AANAT	GCAGTGCAAGATGAAGGACTCTCATAC	AS:i:-16
+      1 AANAT_3349-AANAT	GTATGAAGCGGAACCGCTCATAGAATGCC	AS:i:-4
+      1 AANAT_3349-AANAT	GTATGAAGCTGAACCTCTCATGGAATGCC	AS:i:-2
+      1 AANAT_3349-AANAT	GTATGAAGCTGAAGCTCTCATAGAATGCC	AS:i:-2",
+                 header = FALSE, sep = "\t")
+
+samplekey <- read.table(text = "sample	dox	tam	rep
+sample1	unt	unt	2", header = TRUE)
+
+# Write temporary files.
+write.table(smp, file = file.sample, row.names = FALSE, col.names = FALSE, sep = "\t", quote = FALSE)
+write.table(samplekey, file = file.samplekey, row.names = FALSE, col.names = TRUE, sep = "\t", quote = FALSE)
+cat(file = file.lib, ">AANAT_2131-AANAT
+GTATGAGGCAGCGAAACTCACTGGCTGCC
+>AANAT_2740-AANAT
+GTATGCCACAGCAGGATGGGGCCCCTGCC
+>AANAT_304-AANAT
+GTATAGAAGGGTACCAGCGCGTCCTTGCC
+>AANAT_3349-AANAT
+GTATGAAGCTGAACCTCTCATAGAATGCC\n")
+cat(file = file.trimreport, "sample1
+2,345,004
+2,319,009
+1,718,789\n")
+
+out <- summaryTable(samkey = file.samplekey,
+                    lib = file.lib,
+                    mapping.rx = "^.*_test_spec\\.txt$",
+                    trimreport = file.trimreport,
+                    output_report = "tar.txt", # test alignment report
+                    output_count = "tcr.txt") # tst count matrix
+
+# compare against this
+out.gs <- structure(list(alignment_report = structure(list(sample = structure(1L, .Label = "sample1", class = "factor"),
+                                                           total = 2345004, trimmed_5 = 2319009, trimmed_3 = 1718789,
+                                                           mapped = 264L, `mapped %` = 0.000113, perfect = 193L, `perfect %` = 8.23e-05,
+                                                           good = 32L, `good %` = 1.36e-05, poor = 39L, `poor %` = 1.66e-05,
+                                                           unmapped = 2344740, `unmapped %` = 1), row.names = c(NA,
+                                                                                                                -1L), .Names = c("sample", "total", "trimmed_5", "trimmed_3",
+                                                                                                                                 "mapped", "mapped %", "perfect", "perfect %", "good", "good %",
+                                                                                                                                 "poor", "poor %", "unmapped", "unmapped %"), class = "data.frame"),
+                         count_matrix = structure(list(shRNA = c("AANAT_2131-AANAT",
+                                                                 "AANAT_2740-AANAT", "AANAT_304-AANAT", "AANAT_3349-AANAT"
+                         ), sample1 = c(1, 0, 80, 144)), .Names = c("shRNA", "sample1"
+                         ), row.names = c(NA, -4L), class = "data.frame")), .Names = c("alignment_report",
+                                                                                       "count_matrix"))
+
+test_that("check the working of summaryTable", {
+  expect_identical(object = out, expected = out.gs)
+})
