78f4f22183030c7efc887062cdeed3cb4e755614
Roman Luštrik <romunov@gmail.com>
2018-10-15 17:40:57 +0200

    Improve documentation of makeLibrary function

--- R/makeLibrary.R
+++ R/makeLibrary.R
@@ -1,19 +1,24 @@
 #' Create library of shRNA
 #'
-#' Take a raw file and convert it to a valid fasta format. This file is necessary
-#' for performing alignment.
+#' Take a raw file and convert it to a valid fasta format. This file is necessary for performing
+#' alignment.
 #'
-#' @param input A raw file of three columns (\code{AANAT_1522	AANAT	GTATGGGACTCGGGGATCCCAGGTGTGCC})
-#' where first value is shRNA, second value is gene name and third value is sequence.
+#' @param input A raw file of three columns where first value is shRNA, second value is gene
+#' name and third value is sequence.
 #' @param output Character. Optional. If a character string of length one is supplied, it is
 #' stripped of file extension and appended \code{fasta}. If \code{NULL}, name of input is used,
 #' where \code{.fasta} extension is appended and written to working directory.
 #' @export
-#' @author Roman Lustrik (roman@@genialis.com)
+#' @author Roman Luštrik (roman@@genialis.com) based on original code by Nicholas Neill (nicholas.neill@@bcm.edu).
+#'
+#' @return Returns link to file of the library, which has been written to working directory or
+#' path specified in \code{output} as a side effect.
 
 makeLibrary <- function(input, output = NULL) {
   # Import data.
   xy <- read.table(input, sep = "", header = FALSE)
+  # `input` should look like this:
+  # AANAT_1522 AANAT GTATGGGACTCGGGGATCCCAGGTGTGCC
 
   # Define how a single read for fasta is written.
   out <- sprintf("> %s-%s\n%s", xy[, 1], xy[, 2], xy[, 3])
